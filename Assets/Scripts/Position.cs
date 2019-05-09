40ce3c14dedf62e3040b22819e1075074aef8c07
Theumasu <matheus.o.dalmeida@gmail.com>
2019-05-09 14:26:39 -0300

    V.1

---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class Position : MonoBehaviour
+{
+    void Start()
+    {
+        // deixa o objeto cair
+        gameObject.transform.position = new Vector3(-3, 1, -6);
+    } 
+
+}
