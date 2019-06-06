59a1c1a49432f762aea2f308276bb0c700dd7902
Yolotrs <brunofilype.sb@gmail.com>
2019-06-06 16:51:48 -0300

    Pitch 1

---
+++ Assets/Scripts/BlinkLight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BlinkLight : MonoBehaviour {
+
+  public float timer = 1;
+   public Light light;
+
+   void Start()
+   {
+       StartCoroutine(LightChanging());
+   }
+   
+  IEnumerator LightChanging()
+  {
+      for(;;)
+      {
+      light.enabled = !light.enabled;
+      yield return new WaitForSeconds(timer);
+      }
+
+  }
+   }
---
+++ Assets/Scripts/InitialText.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+public class InitialText : MonoBehaviour {
+ public float tempo = 5.0f;
+ void Update () {
+ tempo -= Time.deltaTime;
+ }
+ void OnGUI () {
+ if(tempo>=0){
+ GUI.Label(new Rect(Screen.width/2-50,Screen.height/2,200,200), "Texo a ser decidido");
+ }
+}
+}
